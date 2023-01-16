9f9a5a80e269c42b227461a53f58c499e9d3d14c
vscode <vscode@codespaces-5c6a0a>
2023-01-16 03:13:43 +0000

    [skip actions] lab/lab18/ch018_t05_instantiating_1st_object.py

--- lab/lab18/ch018_t05_instantiating_1st_object.py
+++ lab/lab18/ch018_t05_instantiating_1st_object.py
@@ -1,7 +1,6 @@
 class Animal(object):
-    def __init__(self, name: str):
+        def __init__(self, name: str, age: int, is_hungry: bool):
         self.name = name
+        self.age = age
+        self.is_hungry = is_hungry
 
-
-zebra = Animal("Jeffrey")
-print(zebra.name)
