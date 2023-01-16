b337b19794956761eb789c7460584c62d8963039
vscode <vscode@codespaces-5c6a0a>
2023-01-16 06:40:36 +0000

    [skip actions] lab/lab19/ch019_t01_class_basics.py

--- lab/lab18/ch018_t12_inheritance_syntax.py
+++ lab/lab18/ch018_t12_inheritance_syntax.py
@@ -1,15 +1,15 @@
 class Shape(object):
     """Makes shapes!"""
 
-    def ___init___(self, number_of_sides: int):
+    def __init__(self, number_of_sides: int):
         self.number_of_sides = number_of_sides
 
 # Add your Triangle class below!
 
 
 class Triangle(Shape):
-    def __init___(self, side1: int, side2: int, side3: int):
-        super().___init___(3)
+    def __init__(self, side1: int, side2: int, side3: int):
+        super().__init__(3)
         self.side1 = side1
         self.side2 = side2
         self.side3 = side3
