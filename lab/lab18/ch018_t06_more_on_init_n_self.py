8e0b75c4f04fc7eb50abbf13ccdfc10eecab1b0c
vscode <vscode@codespaces-5c6a0a>
2023-01-16 03:13:05 +0000

    [skip actions] lab/lab18/ch018_t06_more_on_init_n_self.py

--- lab/lab18/ch018_t06_more_on_init_n_self.py
+++ lab/lab18/ch018_t06_more_on_init_n_self.py
@@ -1,9 +1,6 @@
 # Class definition
 class Animal(object):
-    def __init__(self, name: str, age: int, is_hungry: bool):
-        self.name = name
-        self.age = age
-        self.is_hungry = is_hungry
+    """Makes cute animals."""
 
     # For initializing our instance objects
     def __init__(self, name: str, age: int, is_hungry: bool):
