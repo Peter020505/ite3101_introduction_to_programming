e985145246ac5f9ff117fd695f75341df32cad94
vscode <vscode@codespaces-5c6a0a>
2023-01-16 03:30:38 +0000

    [skip actions] lab/lab18/ch018_t09_they_are_multiplying.py

--- lab/lab18/ch018_t09_they_are_multiplying.py
+++ lab/lab18/ch018_t09_they_are_multiplying.py
@@ -1,7 +1,8 @@
 class Animal(object):
     """Makes cute animals."""
     is_alive = True
-    def _
+    def __init__(self) -> None:
+        pass
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
