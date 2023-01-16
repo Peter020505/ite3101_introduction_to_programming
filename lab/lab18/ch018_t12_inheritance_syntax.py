5f9ef726a2c987589fe6e29ba81431ad12c69595
vscode <vscode@codespaces-5c6a0a>
2023-01-16 04:17:36 +0000

    [skip actions] lab/lab18/ch018_t12_inheritance_syntax.py

--- lab/lab18/ch018_t12_inheritance_syntax.py
+++ lab/lab18/ch018_t12_inheritance_syntax.py
@@ -8,7 +8,7 @@ class Shape(object):
 
 
 class Triangle(Shape):
-    def ___init___(side1: int, side2: int, side3: int):
+    def ___init___(self, side1: int, side2: int, side3: int):
         super().___init___(3)
         self.side1 = side1
         self.side2 = side2
